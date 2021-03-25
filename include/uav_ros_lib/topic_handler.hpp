2b21c547065c2e61f23b92f46c3376079f17a4d5
Lovro Markovic <lovro.markovic@fer.hr>
2021-03-25 17:20:26 +0100

    [TopicHandler] Update

--- include/uav_ros_lib/topic_handler.hpp
+++ include/uav_ros_lib/topic_handler.hpp
@@ -3,6 +3,8 @@
 
 #include <ros/ros.h>
 #include <functional>
+#include <mutex>
+#include <memory>
 
 namespace ros_util {
 
@@ -16,6 +18,14 @@ namespace ros_util {
 template<class T> class TopicHandler
 {
 public:
+  using Ptr = std::unique_ptr<TopicHandler<T>>;
+
+  /**
+   * @brief Construct a new Topic Handler object. Does nothing.
+   *
+   */
+  TopicHandler() {}
+
   /**
    * @brief Construct a new Topic Handler object.
    *
@@ -27,8 +37,26 @@ public:
   TopicHandler(ros::NodeHandle &t_nh,
     std::string t_topicName,
     const double t_timeoutDuration = 2)
+    : TopicHandler(t_nh, t_topicName, nullptr, t_timeoutDuration)
+  {}
+
+  /**
+   * @brief Construct a new Topic Handler object with an additional post callback
+   * function argument.
+   *
+   * @param t_nh A ROS node handle.
+   * @param t_topicName A topic name.
+   * @param t_postCallbackFunction A post callback function.
+   * @param t_timeoutDuration Optional desired timeout duration to indicate topic
+   * inactivity.
+   */
+  TopicHandler(ros::NodeHandle &t_nh,
+    std::string t_topicName,
+    std::function<void(const typename T::ConstPtr &)> t_postCallbackFunction,
+    const double t_timeoutDuration = 2)
     : m_topicName(std::move(t_topicName)), m_topicTimeout(t_timeoutDuration),
-      m_lastMessgeTime(0), m_isResponsive(false), m_messageRecieved(false)
+      m_lastMessgeTime(0), m_isResponsive(false),
+      m_postCallbackFunction(t_postCallbackFunction), m_messageRecieved(false)
   {
     m_subT = t_nh.subscribe(m_topicName, 1, &TopicHandler::callback, this);
 
@@ -39,11 +67,39 @@ public:
   }
 
   /**
+   * @brief Construct a new Topic Handler object with an additional post callback
+   * function argument that can be an object method.
+   *
+   * @tparam Obj Type of the object which holds the callback method
+   * @param t_nh A ROS node handle.
+   * @param t_topicName A topic name.
+   * @param message_callback A post callback function as a class method.
+   * @param object Object upon which the post callback function is called.
+   * @param t_timeoutDuration Optional desired timeout duration to indicate topic
+   * inactivity.
+   */
+  template<typename Obj>
+  TopicHandler(ros::NodeHandle &t_nh,
+    std::string t_topicName,
+    void (Obj::*message_callback)(const typename T::ConstPtr &),
+    Obj *object,
+    const double t_timeoutDuration = 2)
+    : TopicHandler(t_nh,
+        t_topicName,
+        std::bind(message_callback, object, std::placeholders::_1),
+        t_timeoutDuration)
+  {}
+
+  /**
    * @brief Get the latest Data from the topic.
    *
    * @return const T&
    */
-  const T &getData() const { return m_data; }
+  const T &getData() const
+  {
+    std::scoped_lock<std::recursive_mutex> lock(m_recursive_mutex);
+    return *m_data;
+  }
 
   /**
    * @brief Check if the topic is responsive within the specified timeout.
@@ -51,7 +107,11 @@ public:
    * @return true if the topic is responsive.
    * @return false if topic is unresponsive.
    */
-  bool isResponsive() const { return m_isResponsive; }
+  bool isResponsive() const
+  {
+    std::scoped_lock<std::recursive_mutex> lock(m_recursive_mutex);
+    return m_isResponsive;
+  }
 
   /**
    * @brief Check if any message is recieved on the given topic.
@@ -59,18 +119,25 @@ public:
    * @return true if any message is recieved.
    * @return false if no message is recieved.
    */
-  bool isMessageRecieved() const { return m_messageRecieved; }
+  bool isMessageRecieved() const
+  {
+    std::scoped_lock<std::recursive_mutex> lock(m_recursive_mutex);
+    return m_messageRecieved;
+  }
 
 private:
-  void callback(const T &msg)
+  void callback(const typename T::ConstPtr &msg)
   {
-    m_data = std::move(msg);
+    std::scoped_lock<std::recursive_mutex> lock(m_recursive_mutex);
+    m_data = msg;
     m_lastMessgeTime = ros::Time::now().toSec();
     m_messageRecieved = true;
+    if (m_postCallbackFunction) { m_postCallbackFunction(m_data); }
   }
 
   void watchdog_callback(const ros::TimerEvent & /* unused */)
   {
+    std::scoped_lock<std::recursive_mutex> lock(m_recursive_mutex);
     double elapsedTime = ros::Time::now().toSec() - m_lastMessgeTime;
     if (elapsedTime > m_topicTimeout) {
       m_isResponsive = false;
@@ -80,13 +147,15 @@ private:
     }
   }
 
+  std::recursive_mutex m_recursive_mutex;
   ros::Timer m_watchdogTimer;
   double m_topicTimeout, m_lastMessgeTime;
   bool m_isResponsive, m_messageRecieved;
 
   ros::Subscriber m_subT;
-  T m_data;
-  const std::string m_topicName;
+  typename T::ConstPtr m_data;
+  std::string m_topicName;
+  std::function<void(const typename T::ConstPtr &)> m_postCallbackFunction;
 };
 
 
